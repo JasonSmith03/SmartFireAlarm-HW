8013fb8cb957a326c0f58bdad623e04b6124faf4
Bradley Rose <rose.bradley9@gmail.com>
2021-01-07 23:07:05 -0500

    Added README.txt and updated code to coding standards

--- smokeTest.py
+++ smokeTest.py
@@ -10,9 +10,10 @@ from picamera import PiCamera
 from time import sleep
 
 #Threshold values deeming fire to be life threatning
-temperatureThreshold = 30 #actual value: 57
-smokeThreshold = 0.0002 #actual value: 3400
-carbonMonoxideThreshold = 0.0009 #actual value: 35
+#Note that the values found below are test values based on current testing conditions as noted in the project report
+TEMPERATURE_THRESHOLD = 30 #actual value: 57
+SMOKE_THRESHOLD = 0.0002 #actual value: 3400
+CARBON_MONOXIDE_THRESHOLD = 0.0009 #actual value: 35
 
 # create the spi bus
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
@@ -31,80 +32,77 @@ mq = AnalogIn(mcp, MCP.P1)
 camera = PiCamera()
 
 # Function to simplify the math of reading the temperature.
-def tmp36_temperature_C(tempSensor):
-#   print('Raw ADC Value: ', lmt84.value)
-#   print('ADC Voltage: ' + str(lmt84.voltage) + 'V')
-    millivolts = (lmt84.voltage) * 1000
-#     print("VOLTAGE: {}V".format(lmt84.voltage))
-#     print("MILLIVOLTAGE: {}mV".format(millivolts))
-    tempC = ((5.506 - math.sqrt(math.pow(-5.506, 2) + (4 * 0.00176 * (870.6 - millivolts))))/(2 * -0.00176)) + 30 #LMT84 temp sensor transfer function
+def lmt84_temperature_C(tempSensor):
+    '''
+    This is method calculates the temperature of the LMT84 sensor in degrees Celcius using the formula derived from the LMT84 datasheet.
+    '''
+    milliVolts = (lmt84.voltage) * 1000
+    tempC = ((5.506 - math.sqrt(math.pow(-5.506, 2) + (4 * 0.00176 * (870.6 - milliVolts))))/(2 * -0.00176)) + 30 #LMT84 temp sensor transfer function
     return tempC
 
 def calibration(mqSensor):
-    i = 0
-    for i in range(0, 500):
-        mqSensor = mqSensor + mq.value
-        i = i + 1
-    return mqSensor
+    '''
+    This method calibrates the MQ2 sensor to get a sample of clean air during setup. 
+    '''
+    counter = 0
+    for counter in range(0, 500):
+        mqSensorCalibrated = mqSensor + mq.value
+        counter = counter + 1
+    return mqSensorCalibrated
 
 
 mqSensor = 0 #variable to store sensor value
 print("Calibrating...")
-calibrateVal = calibration(mqSensor)
+calibrationValue = calibration(mqSensor)
 time.sleep(5.0)
 #get the average value
-mqAvgVal = calibrateVal / 500
+mqAverageValue = calibrationValue / 500
 #calculate the sensing resistance in clean air
 #3.3 volts and 10RL taken from the datasheet
-rsAir = ((3.3 * 10) / mq.voltage) - 10
-#calculate resistance in clean air from RS using air value
-r0 = rsAir/9.9
-# Loop forever.
+airResistance = (((3.3 * 10) / mq.voltage) - 10)/9.9
 
 while True:
     # Read the temperature in Celsius.
-    temp_C = tmp36_temperature_C(lmt84)
+    temp_C = lmt84_temperature_C(lmt84)
     # Convert to Fahrenheit.
     temp_F = (temp_C * 9/5) + 32
     
-#     LPGm = (math.log10(0.27/0.8))/(math.log10(10000/1000))
-#     LPGb = math.log10(0.8) + LPGm * math.log10(1000)
+    # lpgM = (math.log10(0.27/0.8))/(math.log10(10000/1000))
+    # lpgB = math.log10(0.8) + lpgM * math.log10(1000)
     
-    LPGm = -0.47
-    LPGb = 1.31
+    LPGM = -0.47
+    LPGB = 1.31
     
-    SMOKEm = (math.log10(0.5/1.8))/(math.log10(10000/1000))
-    SMOKEb = math.log10(1.8) + SMOKEm * math.log10(1000)
+    SMOKE_M = (math.log10(0.5/1.8))/(math.log10(10000/1000))
+    SMOKE_B = math.log10(1.8) + SMOKE_M * math.log10(1000)
     
-    COm = (math.log10(1.5/3.1))/(math.log10(10000/1000))
-    COb = math.log10(3.1) + COm * math.log10(1000)
+    CARBON_MONOXIDE_M = (math.log10(1.5/3.1))/(math.log10(10000/1000))
+    CARBON_MONOXIDE_B = math.log10(3.1) + CARBON_MONOXIDE_M * math.log10(1000)
     
     #digital value from detection
     mqValue = mq.value
-    #RS calculation from detection
-    rsGas = ((3.3 * 10) / mq.voltage) - 10
-    #ratio from detection
-    ratio1 = rsGas/r0
-    #log of ratio to calculate ppm
-    ratio = math.log10(ratio1)
+    #Gas resistance calculation from detection
+    gasResistance = ((3.3 * 10) / mq.voltage) - 10
+    #gas to air ppm ratio using log10
+    gasAirRatioPpm = math.log10(gasResistance/airResistance)
     
     #LPG value in ppm
-    LPGratio = (ratio - LPGb)/LPGm
-    LPGppm = math.pow(10, LPGratio)
-    LPGperc = LPGppm / 10000
+    lpgRatio = (gasAirRatioPpm - LPGB)/LPGM
+    lpgPpm = math.pow(10, lpgRatio)
+    lpgPercentage = lpgPpm / 10000
     #Smoke value in ppm
-    SMOKEratio = (ratio - SMOKEb)/SMOKEm
-    SMOKEppm = math.pow(10, SMOKEratio)
-    SMOKEperc = SMOKEppm / 10000
-    #CO value in ppm
-    COratio = (ratio - COb)/COm
-    COppm = math.pow(10, COratio)
-    COperc = COppm / 10000
+    smokeRatio = (gasAirRatioPpm - SMOKE_B)/SMOKE_M
+    smokePpm = math.pow(10, smokeRatio)
+    smokePercentage = smokePpm / 10000
+    #CarbonMonoxide value in ppm
+    carbonMonoxideRatio = (gasAirRatioPpm - CARBON_MONOXIDE_B)/CARBON_MONOXIDE_M
+    carbonMonoxidePpm = math.pow(10, carbonMonoxideRatio)
+    carbonMonoxidePercentage = carbonMonoxidePpm / 10000
 
     #start camera once dangerous values are read
-    if (temp_C >= temperatureThreshold or SMOKEppm >= smokeThreshold or COppm >= carbonMonoxideThreshold):
+    if (temp_C >= TEMPERATURE_THRESHOLD or smokePpm >= SMOKE_THRESHOLD or carbonMonoxidePpm >= CARBON_MONOXIDE_THRESHOLD):
         camera.start_preview()
-        camera.start_recording('/home/pi/SmartFire/VideoFiles/video.h264') #CHANGE DIRECTORY
+        camera.start_recording('/home/pi/SmartFire/VideoFiles/video.h264')
         #note this is currently for testing purposes, will need to define rule set for how long camera stays on and when it turns off
         sleep(10)
         camera.stop_recording()
@@ -112,9 +110,9 @@ while True:
     
     # Print out the value and delay a second before looping again.
     print("Temperature: {}C {}F".format(temp_C, temp_F))
-#     print("LPG%: {}%, LPGppm: {}ppm".format(LPGperc, LPGppm))
-    print("COppm: {}ppm, SMOKEppm: {}ppm, LPGppm: {}ppm".format(COppm, SMOKEppm, LPGppm))
-    print("CO%: {}%, SMOKE%: {}%, LPG%: {}%".format(COperc, SMOKEperc, LPGperc))
+    # print("LPG%: {}%, LPGppm: {}ppm".format(LPGperc, LPGppm))
+    print("COppm: {}ppm, smokePpm: {}ppm, lpgPpm: {}ppm".format(carbonMonoxidePpm, smokePpm, lpgPpm))
+    print("CO%: {}%, SMOKE%: {}%, LPG%: {}%".format(carbonMonoxidePercentage, smokePercentage, lpgPercentage))
     print()
     print()
     time.sleep(1.0)
